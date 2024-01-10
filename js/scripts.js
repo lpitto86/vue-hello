836dc73f2be7a26c5ea04e7317a2561aa1b13d56
lpitto86 <luca.p481@gmail.com>
2024-01-10 17:54:00 +0100

    Messaggio

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+      // messaggio visualizzato
+      const messageData = "Buon Anno Nuovo!";
+  
+      // elemento h1
+      const h1Element = document.getElementById('messaggio');
+  
+      // testo dell'elemento h1
+      h1Element.textContent = messageData;
+});
